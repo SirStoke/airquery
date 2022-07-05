71117e328128157dd0d74bd59640a5b240bf8bc4
Sandro Mosca <sandro.mosca.dev@gmail.com>
2022-07-05 16:13:19 +0200

    Implement a TableScan node for Airtable

--- src/dataflow.rs
+++ src/dataflow.rs
@@ -1,34 +1,50 @@
-use std::io;
 use async_trait::async_trait;
+use serde_json::Value;
+use std::collections::VecDeque;
 use thiserror::Error;
+
+mod connectors;
 
 #[derive(Error, Debug)]
 enum OpenError {
     #[error("node internally failed to setup")]
-    Internal(#[from] io::Error)
+    Internal(#[from] anyhow::Error),
 }
 
 #[derive(Error, Debug)]
 enum NextError {
     #[error("node internally failed to produce a value")]
-    Internal(#[from] io::Error)
+    Internal(#[from] anyhow::Error),
 }
 
 #[derive(Error, Debug)]
 enum CloseError {
     #[error("node internally failed to close")]
-    Internal(#[from] io::Error)
+    Internal(#[from] anyhow::Error),
+}
+
+/// Represents some data found in a Tuple - e.g. a number or a text
+enum Datum {
+    Number(f64),
+    Text(String),
 }
 
 struct Tuple {
-    columns: Vec<String>
+    column_names: Vec<String>,
+    data: Vec<Datum>,
+}
+
+impl Tuple {
+    fn new(column_names: Vec<String>, data: Vec<Datum>) -> Tuple {
+        Tuple { column_names, data }
+    }
 }
 
 /// Represents the physical properties describing the tuples emitted by the Node.
 /// e.g. columns, sorting, etc.
 struct Properties {
     sort_by: Vec<String>,
-    columns: Vec<String>
+    columns: Vec<String>,
 }
 
 /// A Data Flow Node
@@ -38,7 +54,77 @@ struct Properties {
 /// #open() must be called before #next(), which must be called before #close()
 #[async_trait]
 trait Node {
-    async fn open() -> Result<OpenError, ()>;
-    async fn next() -> Result<NextError, Option<Tuple>>;
-    async fn close() -> Result<CloseError, ()>;
+    async fn open(&mut self) -> Result<(), OpenError>;
+    async fn next(&mut self) -> Result<Option<Tuple>, NextError>;
+    async fn close(&mut self) -> Result<(), CloseError>;
+}
+
+const BATCH_SIZE: u16 = 100;
+
+struct AirtableScanNode {
+    table: String,
+    base: String,
+    api_key: String,
+    buffer: VecDeque<Tuple>,
+    curr_offset: u16,
+}
+
+impl AirtableScanNode {
+    async fn load_buffer(&mut self) -> anyhow::Result<()> {
+        let records = connectors::airtable::records(
+            &self.table,
+            &self.base,
+            &self.api_key,
+            BATCH_SIZE,
+            self.curr_offset,
+        )
+        .await?;
+
+        for record in records.records.iter() {
+            let mut column_names = Vec::with_capacity(record.fields.len());
+            let mut data = Vec::with_capacity(record.fields.len());
+
+            for (column, value) in record.fields.iter() {
+                let datum = match value {
+                    Value::Number(number) if number.is_f64() => {
+                        Some(Datum::Number(number.as_f64().unwrap()))
+                    }
+                    Value::String(string) => Some(Datum::Text(string.clone())),
+                    _ => None,
+                };
+
+                if let Some(datum) = datum {
+                    column_names.push(column.clone());
+                    data.push(datum);
+                }
+            }
+
+            self.buffer.push_front(Tuple::new(column_names, data));
+        }
+
+        self.curr_offset += BATCH_SIZE;
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl Node for AirtableScanNode {
+    async fn open(&mut self) -> Result<(), OpenError> {
+        AirtableScanNode::load_buffer(self).await?;
+
+        Ok(())
+    }
+
+    async fn next(&mut self) -> Result<Option<Tuple>, NextError> {
+        if self.buffer.is_empty() {
+            AirtableScanNode::load_buffer(self).await?;
+        }
+
+        Ok(self.buffer.pop_front())
+    }
+
+    async fn close(&mut self) -> Result<(), CloseError> {
+        Ok(())
+    }
 }
---
+++ src/dataflow/connectors.rs
@@ -0,0 +1,42 @@
+pub(super) mod airtable {
+    use anyhow::Result;
+    use reqwest::{Method, Url};
+    use serde::Deserialize;
+    use serde_json::Value;
+    use std::collections::HashMap;
+
+    #[derive(Deserialize)]
+    pub(crate) struct Record {
+        pub id: String,
+        pub created_time: String,
+        pub fields: HashMap<String, Value>,
+    }
+
+    #[derive(Deserialize)]
+    pub(crate) struct Records {
+        pub(crate) records: Vec<Record>,
+    }
+
+    pub(crate) async fn records(
+        table: &str,
+        base: &str,
+        api_key: &str,
+        page_size: u16,
+        offset: u16,
+    ) -> Result<Records> {
+        let client = reqwest::Client::new();
+
+        let url = Url::parse(&format!("https://api.airtable.com/v0/{}/{}", base, table))?;
+
+        let request = client
+            .request(Method::GET, url)
+            .bearer_auth(api_key)
+            .query(&[
+                ("offset", offset.to_string()),
+                ("page_size", page_size.to_string()),
+            ])
+            .build()?;
+
+        Ok(client.execute(request).await?.json::<Records>().await?)
+    }
+}
