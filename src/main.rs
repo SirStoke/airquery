fc653c6f9d666c0f69cef94f10e2a7302d85d941
Sandro Mosca <sandro.mosca.dev@gmail.com>
2022-07-09 15:39:21 +0200

    Introduce clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 mod dataflow;
 
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, name = "Airquery", long_about = None)]
+struct Args {
+    #[clap(env = "AIRQUERY_API_KEY")]
+    airtable_api_key: String,
+    #[clap(env = "AIRQUERY_BASE")]
+    airtable_base: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Args::parse();
+
+    let records = dataflow::connectors::airtable::records(
+        "Transactions",
+        &cli.airtable_base,
+        &cli.airtable_api_key,
+        100,
+        0,
+    )
+    .await?;
+
+    println!("{:?}", records);
+
+    Ok(())
 }
