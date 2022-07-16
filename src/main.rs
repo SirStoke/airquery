229477d547178f4f312eaf0778f042d93d00d71a
Sandro Mosca <sandro.mosca.dev@gmail.com>
2022-07-16 16:40:33 +0200

    Introduce sqlparser

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ mod dataflow;
 
 use clap::Parser;
 
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser as SQLParser;
+
 #[derive(Parser)]
 #[clap(author, version, about, name = "Airquery", long_about = None)]
 struct Args {
-    #[clap(env = "AIRQUERY_API_KEY")]
+    query: String,
+    #[clap(env = "AIRQUERY_API_KEY", short = 'k', long)]
     airtable_api_key: String,
-    #[clap(env = "AIRQUERY_BASE")]
+    #[clap(env = "AIRQUERY_BASE", short = 'b', long)]
     airtable_base: String,
 }
 
@@ -25,6 +29,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await?;
 
     println!("{:?}", records);
+
+    let ast = SQLParser::parse_sql(&GenericDialect {}, &cli.query);
+
+    println!("AST: {:?}", ast);
 
     Ok(())
 }
