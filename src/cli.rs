7c7bad1c8fa2183eec09d490e22e8ea0dc47c32e
Sandro Mosca <sandro.mosca.dev@gmail.com>
2022-07-16 18:15:57 +0200

    Introduce the `Cli` struct and the `compiler` mod
    - The `Cli` struct is meant to be used as the app entrypoint, collecting
      all the necessary data types and methods that dispatch work to more
      specific components
    
    - The `compiler` mod will eventually contain types and methods related
      to compiling the SQL AST coming from sqlparser to physical properties,
      to be passed to a planner

---
+++ src/cli.rs
@@ -0,0 +1,70 @@
+use clap::Parser;
+use sqlparser::ast::Statement;
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::{Parser as SQLParser, ParserError};
+use std::io::Write;
+use std::process::exit;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+#[derive(Parser)]
+#[clap(author, version, about, name = "airquery", long_about = None)]
+struct Args {
+    query: String,
+    #[clap(env = "AIRQUERY_API_KEY", short = 'k', long)]
+    airtable_api_key: String,
+    #[clap(env = "AIRQUERY_BASE", short = 'b', long)]
+    airtable_base: String,
+}
+
+struct Colors {
+    error: ColorSpec,
+    reset: ColorSpec,
+}
+
+pub struct Cli {
+    stderr: StandardStream,
+    args: Args,
+    colors: Colors,
+}
+
+/// Entrypoint for the app
+impl Cli {
+    pub fn new() -> Cli {
+        let args = Args::parse();
+        let stderr = StandardStream::stderr(ColorChoice::Auto);
+
+        let mut colors = Colors {
+            error: ColorSpec::new(),
+            reset: ColorSpec::new(),
+        };
+
+        colors.error.set_fg(Some(Color::Red));
+        colors.error.set_fg(None);
+
+        Cli {
+            stderr,
+            args,
+            colors,
+        }
+    }
+
+    /// Executes the query
+    pub fn execute(&mut self) {
+        match SQLParser::parse_sql(&GenericDialect {}, &self.args.query) {
+            Err(ParserError::ParserError(err)) => self.quit_because(err),
+            Err(ParserError::TokenizerError(err)) => self.quit_because(err),
+            Ok(ast) => println!("AST: {:?}", ast),
+        }
+    }
+
+    /// quits the process because of an error message
+    fn quit_because(&mut self, msg: String) -> ! {
+        self.stderr.set_color(&self.colors.error).unwrap();
+        write!(self.stderr, "Error: ").unwrap();
+
+        self.stderr.set_color(&self.colors.reset).unwrap();
+        writeln!(self.stderr, "{}", &msg).unwrap();
+
+        exit(-1)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,15 @@
+mod cli;
+mod compiler;
 mod dataflow;
-
-use clap::Parser;
 
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser as SQLParser;
 
-#[derive(Parser)]
-#[clap(author, version, about, name = "Airquery", long_about = None)]
-struct Args {
-    query: String,
-    #[clap(env = "AIRQUERY_API_KEY", short = 'k', long)]
-    airtable_api_key: String,
-    #[clap(env = "AIRQUERY_BASE", short = 'b', long)]
-    airtable_base: String,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cli = Args::parse();
+    let mut app = cli::Cli::new();
 
-    let records = dataflow::connectors::airtable::records(
-        "Transactions",
-        &cli.airtable_base,
-        &cli.airtable_api_key,
-        100,
-        0,
-    )
-    .await?;
-
-    println!("{:?}", records);
-
-    let ast = SQLParser::parse_sql(&GenericDialect {}, &cli.query);
-
-    println!("AST: {:?}", ast);
+    app.execute();
 
     Ok(())
 }
